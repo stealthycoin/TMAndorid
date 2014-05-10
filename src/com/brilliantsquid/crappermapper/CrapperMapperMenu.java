df25998c1631973fe9d3c566196ae6c3fec31441
stealthycoin <john.w.carlyle@gmail.com>
2014-05-09 22:02:33 -0700

    Add works, api level set to 19, some other crap

--- src/com/brilliantsquid/crappermapper/CrapperMapperMenu.java
+++ src/com/brilliantsquid/crappermapper/CrapperMapperMenu.java
@@ -2,14 +2,28 @@ package com.brilliantsquid.crappermapper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.content.Intent;
+import android.view.View;
 
 public class CrapperMapperMenu extends Activity
 {
+
+    ImageButton addButton;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+	addButton = (ImageButton)findViewById(R.id.button3);
+	addButton.setOnClickListener(new View.OnClickListener() {
+		public void onClick(View v) {
+		    Intent intent = new Intent(CrapperMapperMenu.this,CrapperMapperAdd.class);
+		    startActivity(intent);
+		}
+	    });
     }
 }
