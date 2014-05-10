20fca96d8715884d24e90de6d277885b1aa3e773
stealthycoin <john.w.carlyle@gmail.com>
2014-05-09 22:26:29 -0700

    Added an activity for everything

---
+++ src/com/brilliantsquid/crappermapper/CrapperMapperRecent.java
@@ -0,0 +1,15 @@
+package com.brilliantsquid.crappermapper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CrapperMapperRecent extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recent);
+    }
+}
