36024ed8ed7f6ba0e76d76271366160332f4760c
stealthycoin <john.w.carlyle@gmail.com>
2014-05-10 13:11:40 -0700

    Fixed layout on main screen and added a field to the secondary view

--- src/com/brilliantsquid/crappermapper/CrapperMapperAdd.java
+++ src/com/brilliantsquid/crappermapper/CrapperMapperAdd.java
@@ -2,14 +2,20 @@ package com.brilliantsquid.crappermapper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CrapperMapperAdd extends Activity
 {
+    
+    EditText name;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add);
+
+	name = (EditText)findViewById(R.id.editText1);
     }
 }
