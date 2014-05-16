a5d86e4902cf8ed5aff61fe0f9c96a1c3b14c48b
Jeremy Crowe <crowe.jb@gmail.com>
2014-05-15 21:45:40 -0700

    stand-alone http get class

--- src/com/brilliantsquid/crappermapper/CrapperMapperList.java
+++ src/com/brilliantsquid/crappermapper/CrapperMapperList.java
@@ -1,18 +1,6 @@
 package com.brilliantsquid.crappermapper;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
 import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class CrapperMapperList extends Activity
@@ -24,40 +12,7 @@ public class CrapperMapperList extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list);
     }   
-    class RequestTask extends AsyncTask<String, String, String>{
-
-        @Override
-        protected String doInBackground(String... uri) {
-            HttpClient httpclient = new DefaultHttpClient();
-            HttpResponse response;
-            String responseString = null;
-            try {
-                response = httpclient.execute(new HttpGet(uri[0]));
-                StatusLine statusLine = response.getStatusLine();
-                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-                    ByteArrayOutputStream out = new ByteArrayOutputStream();
-                    response.getEntity().writeTo(out);
-                    out.close();
-                    responseString = out.toString();
-                } else{
-                    //Closes the connection.
-                    response.getEntity().getContent().close();
-                    throw new IOException(statusLine.getReasonPhrase());
-                }
-            } catch (ClientProtocolException e) {
-                //TODO Handle problems..
-            } catch (IOException e) {
-                //TODO Handle problems..
-            }
-            return responseString;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            //Do anything with response..
-        }
-    }
+    
 }
 
 
