a86f4699f599374fe0816025699c9c02c8678312
Jeremy Crowe <crowe.jb@gmail.com>
2014-05-15 22:43:06 -0700

    parsed search

--- src/com/brilliantsquid/crappermapper/CrapperMapperList.java
+++ src/com/brilliantsquid/crappermapper/CrapperMapperList.java
@@ -1,16 +1,21 @@
 package com.brilliantsquid.crappermapper;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CrapperMapperList extends Activity
 {
+	String search;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list);
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        
     }   
     
 }
