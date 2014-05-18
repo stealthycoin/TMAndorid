5ee197a28618faa6a02776a5079cbd79757a5056
Jeremy Crowe <crowe.jb@gmail.com>
2014-05-17 22:51:25 -0700

    changed optionsmenu creation

--- src/com/brilliantsquid/crappermapper/CrapperMapperMenu.java
+++ src/com/brilliantsquid/crappermapper/CrapperMapperMenu.java
@@ -1,9 +1,12 @@
 package com.brilliantsquid.crappermapper;
 
 import android.app.Activity;
+import android.app.SearchManager;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 import android.widget.TextView;
+import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -68,8 +71,15 @@ public class CrapperMapperMenu extends Activity
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
+    	MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.activity_main_actions, menu);
+ 
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.action_search)
+                .getActionView();
+        searchView.setSearchableInfo(searchManager
+                .getSearchableInfo(getComponentName()));
  
         return super.onCreateOptionsMenu(menu);
     }
