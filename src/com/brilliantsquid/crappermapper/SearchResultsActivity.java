49c7fb061174c078488c5333f7f93d5ba56d4a00
Jeremy Crowe <crowe.jb@gmail.com>
2014-05-18 00:00:54 -0700

    corrected searchresults activity

--- src/com/brilliantsquid/crappermapper/SearchResultsActivity.java
+++ src/com/brilliantsquid/crappermapper/SearchResultsActivity.java
@@ -1,49 +1,74 @@
 package com.brilliantsquid.crappermapper;
 
-import android.app.ActionBar;
-import android.app.Activity;
 import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
-public class SearchResultsActivity extends Activity {
+public class SearchResultsActivity extends ActionBarActivity {
+
 	private TextView txtQuery;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_search_results);
-		// get the action bar
-		ActionBar actionBar = getActionBar();
+		 super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_search_results);
+	 
+	        // get the action bar
+	        android.app.ActionBar actionBar = getActionBar();
+	 
+	        // Enabling Back navigation on Action Bar icon
+	        actionBar.setDisplayHomeAsUpEnabled(true);
+	 
+	        txtQuery = (TextView) findViewById(R.id.txtQuery);
+	 
+	        handleIntent(getIntent());
+	}
 
-		// Enabling Back navigation on Action Bar icon
-		actionBar.setDisplayHomeAsUpEnabled(true);
-		txtQuery = (TextView) findViewById(R.id.txtQuery);
-		handleIntent(getIntent());
-    }
 	@Override
-	protected void onNewIntent(Intent intent) {
-		setIntent(intent);
-		handleIntent(intent);
-    }
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleIntent(intent);
+    }
 	
-	/**
-     * Handling intent data
- 	*/
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.search_results, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
 	private void handleIntent(Intent intent) {
-	if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-		String query = intent.getStringExtra(SearchManager.QUERY);
-			
-			/**
-             * Use this query to display search results like 
-             * 1. Getting the data from SQLite and showing in listview 
-             * 2. Making webrequest and displaying the data 
-             * For now we just display the query only
-             */
-		txtQuery.setText("Search Query: " + query);
- 
-        }
- 
-    }
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+ 
+            /**
+             * Use this query to display search results like 
+             * 1. Getting the data from SQLite and showing in listview 
+             * 2. Making webrequest and displaying the data 
+             * For now we just display the query only
+             */
+            txtQuery.setText("Search Query: " + query);
+ 
+        }
+ 
+    }
+
+
 }
