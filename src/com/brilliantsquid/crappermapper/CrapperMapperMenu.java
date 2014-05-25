9de1ee8de4c9f8b1dff43c543dfd143652ccf30d
Jeremy Crowe <crowe.jb@gmail.com>
2014-05-24 20:05:18 -0700

    removed the menu button listeners from the main class.

--- src/com/brilliantsquid/crappermapper/CrapperMapperMenu.java
+++ src/com/brilliantsquid/crappermapper/CrapperMapperMenu.java
@@ -1,14 +1,11 @@
 package com.brilliantsquid.crappermapper;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
 
 public class CrapperMapperMenu extends BaseActivity
 {
 
-    ImageButton emergencyButton, listButton, addButton, recentButton, locationButton, userButton;
+    
 
     /** Called when the activity is first created. */
     @Override
@@ -17,49 +14,7 @@ public class CrapperMapperMenu extends BaseActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        emergencyButton = (ImageButton)findViewById(R.id.button1);
-        emergencyButton.setOnClickListener(new View.OnClickListener() {
-		public void onClick(View v) {
-		    Intent intent = new Intent(CrapperMapperMenu.this,CrapperMapperEmergency.class);
-		    startActivity(intent);
-		}
-	    });
-	listButton = (ImageButton)findViewById(R.id.button2);
-	listButton.setOnClickListener(new View.OnClickListener() {
-		public void onClick(View v) {
-		    Intent intent = new Intent(CrapperMapperMenu.this,CrapperMapperList.class);
-		    intent.putExtra("key", "");
-		    startActivity(intent);
-		}
-	    });
-	addButton = (ImageButton)findViewById(R.id.button3);
-	addButton.setOnClickListener(new View.OnClickListener() {
-		public void onClick(View v) {
-		    Intent intent = new Intent(CrapperMapperMenu.this,CrapperMapperAdd.class);
-		    startActivity(intent);
-		}
-	    });
-	recentButton = (ImageButton)findViewById(R.id.button4);
-	recentButton.setOnClickListener(new View.OnClickListener() {
-		public void onClick(View v) {
-		    Intent intent = new Intent(CrapperMapperMenu.this,CrapperMapperRecent.class);
-		    startActivity(intent);
-		}
-	    });
-	locationButton = (ImageButton)findViewById(R.id.button5);
-	locationButton.setOnClickListener(new View.OnClickListener() {
-		public void onClick(View v) {
-		    Intent intent = new Intent(CrapperMapperMenu.this,CrapperMapperLocation.class);
-		    startActivity(intent);
-		}
-	    });
-	userButton = (ImageButton)findViewById(R.id.button6);
-	userButton.setOnClickListener(new View.OnClickListener() {
-		public void onClick(View v) {
-		    Intent intent = new Intent(CrapperMapperMenu.this,CrapperMapperUser.class);
-		    startActivity(intent);
-		}
-	    });
+        
     }
     
 
