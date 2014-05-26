49f2cb72b21833ff86a7b94138b0b138f6077749
Jeremy Crowe <crowe.jb@gmail.com>
2014-05-26 03:01:30 -0700

    tried to get post request working. Currently it crashes on creating the input stream. Finalizing bottom action bar

--- src/com/brilliantsquid/crappermapper/CrapperMapperMenu.java
+++ src/com/brilliantsquid/crappermapper/CrapperMapperMenu.java
@@ -1,5 +1,11 @@
 package com.brilliantsquid.crappermapper;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.util.Log;
 
@@ -8,20 +14,40 @@ public class CrapperMapperMenu extends BaseActivity implements GetCallbackInterf
 
     QuerySingleton qs;
 
-    /** Called when the activity is first created. */
+    /** Called when the activity is first created. 
+     * @throws JSONException */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
         
-        qs = QuerySingleton.getInstance(this);
-        qs.sendGet("signin", this);
+        server_request();
     }
 
 	@Override
 	public void onDownloadFinished(String result) {
-		Log.v("HEY GUYS!", result);
+		Log.v("filter1", result);
+	}
+	
+	public void server_request() {
+		Map<String,String> variables = new HashMap<String, String>();
+		JSONObject obj=new JSONObject();
+		try {
+			//obj.put("name__icontains","barn");
+			obj.put("start",0);
+			obj.put("end",10);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		variables.put("filters", obj.toString());
+		
+        //Log.v("filter1", variables.toString());
+        
+        qs = QuerySingleton.getInstance(this);
+        //qs.sendGet("signin", this);
+        qs.sendPost("api/Toilet/get", variables, this);
 	}
 }
