bd2ab3ac0cbcd1fdf5642ea5ff10c9f8795a8a05
Jacob <jateal@ucsc.edu>
2014-05-28 13:02:41 -0700

    added current location for map

--- src/com/brilliantsquid/crappermapper/CrapperMapperLocation.java
+++ src/com/brilliantsquid/crappermapper/CrapperMapperLocation.java
@@ -1,13 +1,18 @@
 package com.brilliantsquid.crappermapper;
 
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.location.LocationClient;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 import android.annotation.TargetApi;
 import android.app.Activity;
@@ -30,6 +35,7 @@ GooglePlayServicesClient.OnConnectionFailedListener
 {    
  // Google Map
     private GoogleMap googleMap;
+
     private Location currentLocation;
     private double lat;
     private double lng;
@@ -63,6 +69,7 @@ GooglePlayServicesClient.OnConnectionFailedListener
 
             googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
+
             
             if (location != null){
             	lat = location.getLatitude();
@@ -76,6 +83,7 @@ GooglePlayServicesClient.OnConnectionFailedListener
             	Log.v("HEY", "FUCKER");
             }
             googleMap.setMyLocationEnabled(true); // false to disable
+
             // check if map is created successfully or not
             if (googleMap == null) {
                 Toast.makeText(getApplicationContext(),
@@ -83,9 +91,36 @@ GooglePlayServicesClient.OnConnectionFailedListener
                         .show();
             }
         }
+        /*
+*/
+        
     }
-
 	
+    private final class MyLocationListener implements LocationListener {
+    	
+        @Override
+        public void onLocationChanged(Location locFromGps) {
+            // called when the listener is notified with a location update from the GPS
+        	currentLocation = locFromGps;
+        }
+	
+        @Override
+        public void onProviderDisabled(String provider) {
+	    // called when the GPS provider is turned off (user turning off the GPS on the phone)
+        }
+	
+        @Override
+        public void onProviderEnabled(String provider) {
+	    // called when the GPS provider is turned on (user turning on the GPS on the phone)
+        }
+	
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+	    // called when the status of the GPS provider changes
+        }
+    }
+	 
+
     @Override
     protected void onResume() {
         super.onResume();
