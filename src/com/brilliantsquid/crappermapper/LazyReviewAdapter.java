1644a277ed30c70fc94538b68bfbdfaef4e06d77
Jeremy Crowe <crowe.jb@gmail.com>
2014-05-30 22:22:20 -0700

    adding proper view linking to lazy review adapter

--- src/com/brilliantsquid/crappermapper/LazyReviewAdapter.java
+++ src/com/brilliantsquid/crappermapper/LazyReviewAdapter.java
@@ -44,52 +44,29 @@ public class LazyReviewAdapter extends BaseAdapter {
 	            vi = inflater.inflate(R.layout.toiletlist, null);
 	        
 	        ArrayList<ImageView> al = new ArrayList<ImageView>();
-	        TextView title = (TextView)vi.findViewById(R.id.toilet); // title
-	        TextView distance = (TextView)vi.findViewById(R.id.distance); //distance
-	        ImageView stars1 = (ImageView)vi.findViewById(R.id.star1); //stars
-	        ImageView stars2 = (ImageView)vi.findViewById(R.id.star2);
-	        ImageView stars3 = (ImageView)vi.findViewById(R.id.star3);
-	        ImageView stars4 = (ImageView)vi.findViewById(R.id.star4);
-	        ImageView stars5 = (ImageView)vi.findViewById(R.id.star5);
-	        TextView reviews = (TextView)vi.findViewById(R.id.reviews); // reviews
-	        ImageView thumb_image=(ImageView)vi.findViewById(R.id.list_image); // thumb image
+	        ImageView stars1 = (ImageView)vi.findViewById(R.id.star1_rev); //stars
+	        ImageView stars2 = (ImageView)vi.findViewById(R.id.star2_rev);
+	        ImageView stars3 = (ImageView)vi.findViewById(R.id.star3_rev);
+	        ImageView stars4 = (ImageView)vi.findViewById(R.id.star4_rev);
+	        ImageView stars5 = (ImageView)vi.findViewById(R.id.star5_rev);
+	        TextView reviews = (TextView)vi.findViewById(R.id.review_reviews); // reviews
+	        TextView review_text = (TextView)vi.findViewById(R.id.review_text);
 	        
 	        
 	        //LOL WTF IS HAPPENING
 	        al.add(stars1);al.add(stars2);al.add(stars3);al.add(stars4);al.add(stars5);
 	        
-	        HashMap<String, String> toilet = new HashMap<String, String>();
-	        toilet = data.get(position);
+	        HashMap<String, String> review = new HashMap<String, String>();
+	        review = data.get(position);
 	        
-	      //Double value of the reviews
-	        double rev = Double.valueOf(toilet.get(CrapperMapperMenu.KEY_STARS));
+	        //Double value of the reviews
+	        double rev = Double.valueOf(review.get(CrapperMapperMenu.KEY_STARS));
 	        
-	        String dist = toilet.get(CrapperMapperMenu.KEY_DISTANCE);
-	        
-	        Log.v("UTIL", "Name:" + toilet.get(CrapperMapperMenu.KEY_TOILET));
+	        //Get review
+	        //String review_text = review.get(CrapperMapperMenu.KEY_TEXT);
 	        Utilities.display_stars(al, rev);
-
-	        boolean male =  Boolean.valueOf(toilet.get(CrapperMapperMenu.KEY_MALE));
-	        boolean female = Boolean.valueOf(toilet.get(CrapperMapperMenu.KEY_FEMALE));
 	        
-	        
-	        
-	        if (male && !female){
-	        	thumb_image.setImageResource(R.drawable.toilet_men);
-	        }
-	        else if (female && !male){
-	        	thumb_image.setImageResource(R.drawable.toilet_women);
-	        }
-	        else {
-	        	thumb_image.setImageResource(R.drawable.toilet_both);
-	        }
-
-	        
-	        // Setting all values in listview
-	        title.setText(toilet.get(CrapperMapperMenu.KEY_TOILET));
-	        
-	        reviews.setText("Reviews: " + toilet.get(CrapperMapperMenu.KEY_REVIEWS));
-	        distance.setText(String.format("%.1f mi", Double.valueOf(dist)));
+	        reviews.setText("Reviews: " + review.get(CrapperMapperMenu.KEY_REVIEWS));
 
 	        return vi;
 	    }
