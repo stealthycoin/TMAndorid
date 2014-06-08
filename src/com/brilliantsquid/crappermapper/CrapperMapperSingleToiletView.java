859f08898c7362fdc3c1419464f12f7356fab5e9
stealthycoin <john.w.carlyle@gmail.com>
2014-06-07 23:18:10 -0700

    Removed useless imports

--- src/com/brilliantsquid/crappermapper/CrapperMapperSingleToiletView.java
+++ src/com/brilliantsquid/crappermapper/CrapperMapperSingleToiletView.java
@@ -1,9 +1,6 @@
 package com.brilliantsquid.crappermapper;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -14,7 +11,6 @@ import org.json.JSONObject;
 
 import android.content.Context;
 import android.content.Intent;
-import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,7 +21,6 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 
 public class CrapperMapperSingleToiletView extends BaseActivity implements GetCallbackInterface, PostCallbackInterface {
 
